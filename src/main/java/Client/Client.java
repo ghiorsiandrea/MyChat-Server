12e2b99609ed784d1aa90d23c33f9b7ea6934eab
ghiorsiandrea <andreaghiorsims@gmail.com>
2022-09-01 15:33:52 -0300

    Exercise one -
    Curso Java. - Pildoras Informaticas Sockets II. Vídeo 191
    
    In order to the Server through the socket listens to the Client at all times, we will use a Tread

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -3,17 +3,17 @@ package Client;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 public class Client {
-
+    public static final int PORT = 9999;
     public static void main(String[] args) {
         MarcoCliente miMarco = new MarcoCliente();
         miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
     }
-
     public static class MarcoCliente extends JFrame {
-
         public MarcoCliente() {
             setBounds(600, 300, 280, 350);
             LaminaMarcoCliente milamina = new LaminaMarcoCliente();
@@ -21,12 +21,9 @@ public class Client {
             setVisible(true);
         }
     }
-
     static class LaminaMarcoCliente extends JPanel {
-
         private JTextField campo1;
         private JButton miboton;
-
         public LaminaMarcoCliente() {
 
             JLabel texto = new JLabel("CLIENT");
@@ -38,17 +35,21 @@ public class Client {
             miboton.addActionListener(mievento);
             add(miboton);
         }
-
         private class EnviaTexto implements ActionListener {
-
             @Override
             public void actionPerformed(ActionEvent e) {
 
                 System.out.println(campo1.getText());
+                try {
+                    Socket misocket = new Socket("127.0.0.1", Client.PORT);
+                    DataOutputStream flujo_salida = new DataOutputStream(misocket.getOutputStream());
+                    flujo_salida.writeUTF(campo1.getText());
+                    flujo_salida.close();
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
         }
-
-
     }
-
 }
--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -1,20 +1,21 @@
 package Server;
 
+import Client.Client;
+
 import javax.swing.*;
 import java.awt.*;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
-
     public static void main(String[] args) {
         MarcoServidor miMarco = new MarcoServidor();
         miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
     }
-
-    public static class MarcoServidor extends JFrame {
-
+    public static class MarcoServidor extends JFrame implements Runnable {
         JTextArea areatexto;
-
         public MarcoServidor() {
             setBounds(1200, 300, 280, 350);
             JPanel milamina = new JPanel();
@@ -23,6 +24,25 @@ public class Server {
             milamina.add(areatexto, BorderLayout.CENTER);
             add(milamina);
             setVisible(true);
+
+            // In order to the Server through the socket listens to the Client at all times, we will use a Tread
+            Thread mihilo = new Thread(this);
+            mihilo.start();
+        }
+        @Override
+        public void run() {
+            System.out.println("FUNCIONA");
+            try {
+                ServerSocket servidor = new ServerSocket(Client.PORT);
+                Socket misocket = servidor.accept();
+                DataInputStream flujo_entrada = new DataInputStream(misocket.getInputStream());
+                String mensaje_texto = flujo_entrada.readUTF();
+                areatexto.append("\n" + mensaje_texto);
+                misocket.close();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
